5a3f02fd235bfa6b605a2c37f9e92e606fb6e4aa
Mudassir Hussain <140676853+mudassircodes@users.noreply.github.com>
2024-04-17 19:16:56 +0500

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Mudassir Hussain — 3D Portfolio",
+    fullName: "Mudassir Hussain",
+    email: "mudassirarid@mail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Mudassir Hussain
+      ",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
